c6aa0b003b9421fe57347aeffbab156e2f6e471a
Dudu D <duduz@users.noreply.github.com>
2014-04-27 10:13:18 +0300

    Code improvments
    validating that variables cannot be empty

--- SCCMSoftwareUpdateGroup.ps1
+++ SCCMSoftwareUpdateGroup.ps1
@@ -2,17 +2,34 @@
 # Dudu D / AgileIT
 #
 
-### Declare Parameters for the script
+### Declare Parameters for the script and validate they are not empty
 Param(
-  [string]$Scan,            # Specify -Scan for scanning updates and retreveing UpdateID's from the SUP
-  [string]$UpdatesFile      # Specify filename of with list of Updates to add into to an exisiting SoftwareUpdateGroup
-  [string]$LogFile          # Specify a path for a log file - will contains SUCCESS or Failed messages
+  [ValidateNotNullorEmpty()]
+  [string]$Scan,
+  [ValidateNotNullorEmpty()]
+  [string]$UpdatesFile,
+  [ValidateNotNullorEmpty()]
+  [string]$SUG,
+  [ValidateNotNullorEmpty()]
+  [string]$Logfile
 )
+
+### Verify PowerShell Version
+$hostVersionInfo = (Get-Host).Version.Major
+If ($hostVersionInfo -lt 3)
+{
+  Throw "You are using PowerShell $hostVersionInfo. In order to run this script it is required to use PowerShell 3 and up."
+}
+
+### Verify that PowerShell instance is x86
+If ([Environment]::Is64BitProcess -eq $True) {
+    Throw "ConfigMgr 2012 can be loaded only in PowerShell x86"
+}
 
 ### Error Handling
 $ErrorActionPreference = "SilentlyContinue"
 
-### Load The SCCM 2012 Management Module
+### Load The SCCM 2012 Module
 Set-ExecutionPolicy RemoteSigned -Force
 $CMModulePath = $Env:SMS_ADMIN_UI_PATH.ToString().SubString(0,$Env:SMS_ADMIN_UI_PATH.Length - 5) + "\ConfigurationManager.psd1" 
 If (-not (Get-Module ConfigurationManager)) { 
@@ -22,6 +39,10 @@ If (-not (Get-Module ConfigurationManager)) {
     Set-Location "$($CMPSDrive):"
 }
 
+
+#$Updates = Get-Content C:\Temp\updates.txt
+#$LogFile = "C:\Temp\PSUpdates.log"
+#$SUG = "Global Updates 04"
 
 If($Scan) { 
     Write-Host "$NULL"
@@ -33,26 +54,40 @@ If($Scan) {
     }
     Else {
         $ScanResults | select LocalizedDisplayName, ArticleID, CI_ID | Format-Table -AutoSize
-        $Scan = $NULL
-        Exit
+    Exit
     }
-   }
+    $Scan = $NULL
+}
 
 
 If (!$UpdatesFile) { 
     Write-Host "$NULL"
-    Write-Host "Please provide a filename with a list of Updates."
-    Write-Host "Syntax usage is: .\SCCMSoftwareUpdateGroup.ps1 -UpdatesFile ""C:\Temp\updates.txt"" -SUG ""Updates for February 2014"" -Logfile ""C:\Temp\PSUpdates.log"""
+    Write-Host "A path to gather a list of updates from must be specifed using -UpdatesFile"
+    Write-Host "Syntax usage is: SCCMUpdatesV1.ps1 -UpdatesFile ""C:\temp\list1.txt"" -SUG ""Software Update Group 5"" -Logfile ""C:\Temp\Logfile.txt"""
     Exit
 }
-
-$Updates = Get-Content $UpdatesFile
-foreach ($Update in $Updates) {
-    Add-CMSoftwareUpdateToGroup -SoftwareUpdateGroupName $SUG -SoftwareUpdateName *$Update* -ErrorAction SilentlyContinue
-    
-    If ($?) {
-        Write-Host -ForegroundColor "Green" -BackgroundColor DarkGreen KB$Update added sucessfully
-        Write-Output "[OK] KB$Update added sucessfully" | Out-File -append -filepath $Logfile }
-    Else {
-        Write-Host -Background "Black" -ForegroundColor "Red" KB$Update failed to add.
-        Write-Output "[BAD] KB$Update Failed." | Out-File -append -filepath $Logfile }
+If (($UpdatesFile) -and (!$SUG)) {
+    Write-Host "$NULL"
+    Write-Host "Please provide a Software Update Group Name using -SUG"
+    Write-Host "Syntax usage is: SCCMUpdatesV1.ps1 -UpdatesFile ""C:\temp\list1.txt"" -SUG ""Software Update Group 5"" -Logfile ""C:\Temp\Logfile.txt"""
+    Exit
+}
+Else {  
+        $CMPSDrive = Get-PSDrive -PSProvider CMSite
+        $Updates = Get-Content $UpdatesFile | ? {$_.trim() -ne "" }
+        foreach ($Update in $Updates) {
+        If ($Logfile) {
+                If ($?) {
+            Write-Host -ForegroundColor "Green" -BackgroundColor DarkGreen KB$Update added sucessfully
+            Write-Output "[OK] KB$Update added sucessfully" | Out-File -append -filepath $Logfile }
+                Else {
+            Write-Host -Background "Black" -ForegroundColor "Red" KB$Update failed to add.
+            Write-Output "[BAD] KB$Update Failed." | Out-File -append -filepath $Logfile }
+        }
+        Else {
+            If ($?) 
+                 { Write-Host -ForegroundColor "Green" -BackgroundColor DarkGreen KB$Update added sucessfully }
+            Else { Write-Host -Background "Black" -ForegroundColor "Red" KB$Update failed to add. }
+        }
+    }
+}
